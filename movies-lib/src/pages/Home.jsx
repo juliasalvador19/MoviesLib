c0439fb24e39bfebabe2c7f9234d2919e22b2153
Julia Salvador <juliasalvador10@gmail.com>
2024-04-02 18:35:21 -0300

    .

--- movies-lib/src/pages/Home.jsx
+++ movies-lib/src/pages/Home.jsx
@@ -1,4 +1,5 @@
 import { useState, useEffect } from 'react';
+import MovieCard from '../components/MovieCard';
 
 const moviesURL = import.meta.env.VITE_API;
 const apiKey = import.meta.env.VITE_API_KEY;
@@ -17,9 +18,18 @@ const Home = () => {
         
         const topRatedUrl = `${moviesURL}top_rated?${apiKey}`;
         getTopRatedMovies(topRatedUrl);
-    }, [])
+    }, []);
 
-    return <div>Home</div>;
+    return ( 
+        <div className="container">
+            <h2 className="title">Melhores filmes:</h2>
+            <div className="movies-conteiner">
+                {topMovies.length === 0 && <p>Carregando...</p>}
+                {topMovies.length > 0 && 
+                    topMovies.map((movie) => <MovieCard key={movie.id} movie={movie} />)}
+            </div>
+        </div>
+    );
 };
 
 export default Home;

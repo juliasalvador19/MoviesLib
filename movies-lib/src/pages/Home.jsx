ff9d14d8f125c2ec7d66f48ed5a198408e96a785
Julia Salvador <juliasalvador10@gmail.com>
2024-04-02 21:25:52 -0300

    Finished Project

--- movies-lib/src/pages/Home.jsx
+++ movies-lib/src/pages/Home.jsx
@@ -1,6 +1,8 @@
 import { useState, useEffect } from 'react';
 import MovieCard from '../components/MovieCard';
 
+import './MoviesGrid.css';
+
 const moviesURL = import.meta.env.VITE_API;
 const apiKey = import.meta.env.VITE_API_KEY;
 
@@ -23,7 +25,7 @@ const Home = () => {
     return ( 
         <div className="container">
             <h2 className="title">Melhores filmes:</h2>
-            <div className="movies-conteiner">
+            <div className="movies-container">
                 {topMovies.length === 0 && <p>Carregando...</p>}
                 {topMovies.length > 0 && 
                     topMovies.map((movie) => <MovieCard key={movie.id} movie={movie} />)}
